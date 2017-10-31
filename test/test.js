0108ceafb9a7a64b49c6b4651b5646a9f657a850
yjhmelody <465402634@qq.com>
2017-10-31 12:20:17 +0800

    update

---
+++ test/test.js
@@ -0,0 +1,20 @@
+const util = require('../lib/util')
+
+let U = [
+    [1, 2, 3],
+    [0, 5, 6],
+    [0, 0, 1]
+]
+let y = [0, 11, 1]
+console.log(util.backSubstitution(U, y))
+
+
+U = [
+    [1, 2, 1],
+    [2, 5, 6],
+    [1, 2, 1]
+]
+
+ y = [2, 11, 1]
+
+console.log(util.catchFunc(U, y))
