f7f34e7cbd8302953598959f53ad9fb92de4dfc3
yjhmelody <465402634@qq.com>
2017-11-01 14:48:13 +0800

    update: guessmethod

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const util = require('../lib/util')
 
+// root = [-5, 1, 1]
 let U = [
     [1, 2, 3],
     [0, 5, 6],
@@ -9,12 +10,19 @@ let y = [0, 11, 1]
 // console.log(util.backSubstitution(U, y))
 
 
+// root = [1, 2, 3]
 U = [
-    [1, 2, 1],
-    [2, 5, 6],
-    [1, 2, 1]
+    [1, 2, 0],
+    [1, 1, 2],
+    [0, 1, 3]
 ]
+y = [5, 9, 11]
+// console.log(util.catchFunc(U, y))
 
- y = [2, 11, 1]
-
-console.log(util.catchFunc(U, y))
+U = [
+    [2, 2, -1],
+    [1, -1, 0],
+    [4, -2, -1]
+]
+y = [-4, 0, -6]
+console.log(util.guassMethod(U, y))
